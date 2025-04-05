3e974435ad8ae4f7b85f42f2f09ea2bf9552f7e1
Lucas Godoy - aú desviante <lucasgodoyjor@gmail.com>
2025-04-05 07:17:47 -0300

    Added main method of task list

--- src/list/operacoesbasicas/ListaTarefa.java
+++ src/list/operacoesbasicas/ListaTarefa.java
@@ -35,4 +35,24 @@ public class ListaTarefa {
     public void obterDescricoesTarefas() {
         System.out.println(tarefaList);
     }
+
+    public static void main(String[] args) {
+        ListaTarefa listaTarefa = new ListaTarefa();
+        System.out.println("O número total de tarefas é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.adicionarTarefa("Tarefa 1: Estudar java");
+        System.out.println("O número total de tarefas é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.adicionarTarefa("Tarefa 2: Praticar Java");
+        System.out.println("O número total de tarefas é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.adicionarTarefa("Tarefa 3: Atualizar Linkedin");
+        System.out.println("O número total de tarefas é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.removerTarefa("Tarefa 3: Atualizar Linkedin");
+        System.out.println("O número total de tarefas é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.obterDescricoesTarefas();
+
+    }
 }
--- src/list/operacoesbasicas/Tarefa.java
+++ src/list/operacoesbasicas/Tarefa.java
@@ -13,4 +13,10 @@ public class Tarefa {
     public String getDescricao() {
         return descricao;
     }
+
+    @Override
+    public String toString() {
+        return "Tarefa: " +
+        descricao + '\'';
+    }
 }
