af7bc179b50e6eac8e64749e6892c6a73ec3c62e
Lucas Godoy - aú desviante <lucasgodoyjor@gmail.com>
2025-04-05 14:12:53 -0300

    added book catalog

---
+++ src/Pesquisa/Livro.java
@@ -0,0 +1,52 @@
+package Pesquisa;
+
+public class Livro {
+
+    // Atributos
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+
+
+    public Livro(String titulo, String autor, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+
+    public String getTitulo() {
+        return this.titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return this.autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnoPublicacao() {
+        return this.anoPublicacao;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+
+    @Override
+    public String toString() {
+        return "{" +
+            " titulo='" + getTitulo() + "'" +
+            ", autor='" + getAutor() + "'" +
+            ", anoPublicacao='" + getAnoPublicacao() + "'" +
+            "}";
+    }
+    
+}
